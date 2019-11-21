b4023559603ad12f22bd955efe9e68e8217b7a14
SimonVestman <simonw202@gmail.com>
2019-11-21 11:09:28 +0100

    Initial commit

---
+++ src/UppgiftB.java
@@ -0,0 +1,16 @@
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.util.Scanner;
+
+public class UppgiftB {
+    public static void main(String[] args){
+
+        double[] a;
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.println("Hur många tal skulle du vilja beräkna medelvärdet av?");
+        int num = sc.nextInt();
+
+    }
+}

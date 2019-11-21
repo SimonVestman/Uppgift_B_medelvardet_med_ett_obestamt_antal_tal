cdd119903f6e2300fc01830c2fd2cdfe555706c2
SimonVestman <simonw202@gmail.com>
2019-11-21 11:17:38 +0100

    asd

--- src/UppgiftB.java
+++ src/UppgiftB.java
@@ -1,16 +1,23 @@
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+/*
+Skapat 2019-11-21
+Av Simon Vestman
+*/
 
 import java.util.Scanner;
 
 public class UppgiftB {
     public static void main(String[] args){
 
-        double[] a;
+        double[] a = new double[5];
         Scanner sc = new Scanner(System.in);
 
-        
+            System.out.println("vilka tal?");
+                for (int i = 0; i<5;i++)
+                    a[i]= sc.nextDouble();
+
         System.out.println("Hur många tal skulle du vilja beräkna medelvärdet av?");
         int num = sc.nextInt();
+
 
     }
 }
